663f81f6749c0ceb630d022a725f704e593cc55c
none159 <noneenone019@gmail.com>
2025-01-12 16:17:08 +0100

    update

--- src/app/api/edgestore/[...edgestore]/route.ts
+++ src/app/api/edgestore/[...edgestore]/route.ts
@@ -1,16 +1,19 @@
 import { initEdgeStore } from "@edgestore/server";
-import {
-  createEdgeStoreNextHandler,
-} from "@edgestore/server/adapters/next/app";
-const es = initEdgeStore.create();
+import { createEdgeStoreNextHandler } from "@edgestore/server/adapters/next/app";
 
+// Initialize Edge Store
+const es = initEdgeStore.create(); // No arguments here
+
+// Define the router
 const edgeStoreRouter = es.router({
-  myPublicImages: es.imageBucket()})
+  myPublicImages: es.imageBucket(),
+});
+
+// Create the handler
 const handler = createEdgeStoreNextHandler({
-    router: edgeStoreRouter,
- 
-  });
-  
-  export { handler as GET, handler as POST };
-  
-  export type EdgeStoreRouter = typeof edgeStoreRouter;
+  router: edgeStoreRouter,
+});
+
+// Export the handlers
+export { handler as GET, handler as POST };
+export type EdgeStoreRouter = typeof edgeStoreRouter;
